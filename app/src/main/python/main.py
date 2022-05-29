9f89ad9940bd5dc5796b16b8d3be2125a4bba464
aryan-2783 <srivastavaakash761@gmail.com>
2022-05-29 10:27:51 +0530

    UPDATED

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -4,6 +4,6 @@ import numpy as np
 def specific():
    filename=join(dirname(__file__),"libs2/cars_engage_2022.csv")
    df=pd.read_csv(filename)
-   XD=df[['Body_Type','Speedometer','Drivetrain','Fuel_Type','Type']].value_counts().idxmax()
-   return(XD)
+   XD=df[['Body_Type','Speedometer','Drivetrain','Fuel_Type','Type']]
+   return(XD.value_counts().idxmax())
 
